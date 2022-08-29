c9719c6a6da6640885c4983a56adffd3ec35f6bb
Daniel Fonseca Alves <dalraf@gmail.com>
2022-08-29 17:43:07 -0300

    Funcionando processamento

--- start.py
+++ start.py
@@ -6,16 +6,25 @@ maxthreads = 3
 sema = threading.Semaphore(value=maxthreads)
 threads = list()
 
-def task(i):
+
+def task(value):
     sema.acquire()
-    list_command = ['echo', rsync_bin, rsync_options, location_list[i]['origin'], location_list[i]['destini']]
-    command = ' '.join(list_command)
-    print(command)
+    nome = value['nome']
+    log_file = f'/var/log/backup{nome}.log'
+    rsync_log_command = f'--log-file={log_file}'
+    list_command = [
+        rsync_bin,
+        rsync_options,
+        rsync_log_command,
+        value["origin"],
+        value["destin"],
+    ]
+    command = " ".join(list_command)
     subprocess.call(command, shell=True)
     sema.release()
 
+
 for indice, value in enumerate(location_list):
-    print(indice)
-    thread = threading.Thread(target=task, args=(str(indice)))
+    thread = threading.Thread(target=task, args=(value,))
     threads.append(thread)
     thread.start()
