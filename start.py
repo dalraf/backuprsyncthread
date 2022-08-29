dc516d2fb48f76b99e217da72ac49cc31e886824
Daniel Fonseca Alves <dalraf@gmail.com>
2022-08-29 17:45:18 -0300

    Adicionado rotate log

--- start.py
+++ start.py
@@ -1,10 +1,35 @@
 import threading
 import subprocess
+import os
+from pathlib import Path
 from config import rsync_bin, rsync_options, location_list
 
 maxthreads = 3
 sema = threading.Semaphore(value=maxthreads)
 threads = list()
+
+def compress_file(file):
+    subprocess.call(f"gzip {file}", shell=True)
+
+def rename_file(file1, file2):
+    os.rename(file1, file2)
+
+
+def rotate_file(filepadrao):
+    file_path = Path(filepadrao)
+    file_list = file_path.parent.rglob(file_path.name + "*")
+    for file_obj in file_list:
+        file = str(file_obj)
+        list_name_file = file.strip(".")
+        if file == filepadrao:
+            compress_file(file)
+        elif file == filepadrao + ".gz":
+            os.rename(file, file + ".1")
+        elif list_name_file[2] in [str(i) for i in range(2, 8)]:
+            os.rename(file, list_name_file[0] + "gz" + str(int(list_name_file[2]) + 1))
+        else:
+            os.remove(file)
+
 
 
 def task(value):
@@ -21,6 +46,7 @@ def task(value):
     ]
     command = " ".join(list_command)
     subprocess.call(command, shell=True)
+    rotate_file(log_file)
     sema.release()
 
 
