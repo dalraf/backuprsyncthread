b8b7de93c59b7b5c02aa73a83bf3bdd916c2a6c0
Daniel Fonseca Alves <dalraf@gmail.com>
2022-09-02 13:12:58 -0300

    Definido novo modo de rotate

--- start.py
+++ start.py
@@ -1,35 +1,13 @@
 import threading
 import subprocess
-import os
 from pathlib import Path
+from logging.handlers import RotatingFileHandler
 from config import rsync_bin, rsync_options, location_list
+
 
 maxthreads = 3
 sema = threading.Semaphore(value=maxthreads)
 threads = list()
-
-def compress_file(file):
-    subprocess.call(f"gzip {file}", shell=True)
-
-def rename_file(file1, file2):
-    os.rename(file1, file2)
-
-
-def rotate_file(filepadrao):
-    file_path = Path(filepadrao)
-    file_list = file_path.parent.rglob(file_path.name + "*")
-    for file_obj in file_list:
-        file = str(file_obj)
-        list_name_file = file.strip(".")
-        if file == filepadrao:
-            compress_file(file)
-        elif file == filepadrao + ".gz":
-            os.rename(file, file + ".1")
-        elif list_name_file[3] in [str(i) for i in range(2, 20)]:
-            os.rename(file, list_name_file[0] + "gz" + str(int(list_name_file[3]) + 1))
-        else:
-            os.remove(file)
-
 
 
 def task(value):
@@ -45,8 +23,9 @@ def task(value):
         value["destin"],
     ]
     command = " ".join(list_command)
-    rotate_file(log_file)
     subprocess.call(command, shell=True)
+    rotate_log = RotatingFileHandler(log_file, backupCount=20)
+    rotate_log.doRollover()
     sema.release()
 
 
